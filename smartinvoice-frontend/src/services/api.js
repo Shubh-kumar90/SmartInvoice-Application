bd2560cba4957a044cc039de681f95485ac276f7
Shubham Prajapati <shubh.prajapati90@gmail.com>
2026-04-29 01:14:02 +0530

    Updated CORS for deployment

--- smartinvoice-frontend/src/services/api.js
+++ smartinvoice-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL:"https://smartinvoice-wqhx.onrender.com"
 });
 
 API.interceptors.request.use((config) => {
